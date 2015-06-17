413c9cc5d9c5b70fba04fc8733f9b089b00dd3ac
laihaiming <laihaiming@gmail.com>
2015-06-17 12:37:35 +1000

    If there's an error, save the record to make the table

--- scraper.rb
+++ scraper.rb
@@ -21,7 +21,7 @@ page.at('div.content-spacing').search('p').each do |p|
     'date_scraped'      => Date.today.to_s
   }
   
-  if ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? 
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true) 
     ScraperWiki.save_sqlite(['council_reference'], record)
   else
      puts "Skipping already saved record " + record['council_reference']
